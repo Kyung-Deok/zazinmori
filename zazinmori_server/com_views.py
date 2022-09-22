3b7d5b0a4cdb65343b6a7cb4c701def16caecc90
nijes <monochrome05@gmail.com>
2022-09-22 13:54:44 +0900

    'edit_views'

---
+++ zazinmori_server/com_views.py
@@ -0,0 +1,115 @@
+from pprint import pprint
+import django
+from time import time
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.db.models import Sum, Avg,Q
+
+from .models import *
+import datetime
+
+# 기업 검색 결과 : 기업 기본정보, 재무정보, 현재 채용 정보
+def search_company(request):
+    context={}
+    if request.method == "GET":
+        context['message'] = "search company"
+        context['user_email'] = request.session.get('user_email', '')
+        return render(request, 'search.html', {'context':context})
+    elif request.method == "POST" :
+        #try:
+        req_corp_nm = request.POST.get('corp_nm', False)
+        com_info = Corporation.objects.filter(corp_nm__contains=req_corp_nm)
+        context['corp_num'] = len(com_info)
+        corps = {}
+        for i in range(len(com_info)):
+            corp_dict = {}
+            corp_dict['corp_nm'] = com_info[i].corp_nm
+            corp_dict['regi_code'] = com_info[i].regi_code
+            corp_dict['address'] = com_info[i].address
+            corp_dict['ceo_nm'] = com_info[i].ceo_nm
+            corp_dict['est_dt'] = com_info[i].est_dt
+            corp_dict['category'] = com_info[i].category
+            corps[f'{i}'] = corp_dict
+        context['corps'] = corps
+
+        return JsonResponse(context, status=200)
+
+
+
+def company_detail(request):
+    context = {}
+
+    req_regi_code = request.POST['regi_code']
+
+    com_info = Corporation.objects.filter(regi_code=req_regi_code)
+    context['com_info'] = com_info.values()[0]
+
+    try:
+        com_finance = Corp_finance.objects.filter(regi_code=req_regi_code)
+        context['com_finance'] = com_finance.values()[0]
+    except:
+        context['com_finance'] = ''
+
+    try:
+        com_recruits = Jobposting.objects.filter(regi_code=req_regi_code)
+        context['recruits'] = com_recruits.values()[0]
+    except:
+        context['recruits'] = ''
+
+    return JsonResponse(context, status=200)
+    # except django.db.utils.OperationalError :
+    #     return JsonResponse({'err':"테이블 없음"}, status=400)
+    # except Exception as err:
+    #     return JsonResponse({"err": err})
+
+
+def recruit_company(request, jobposting_id):
+    context={}
+    if request.method == "GET":
+        # return render(request, '채용 공고 정보 페이지.html')
+        try:
+            context['message'] = f"채용 공고 정보 페이지 {jobposting_id}번 회사 채용공고"
+            req_corp_nm = request.GET.get('corp_nm', None)
+            
+            if req_corp_nm is None :
+                return redirect('/')
+            
+            com_info = Corporation.objects.filter(corp_nm=req_corp_nm)
+            # job_postings = Job_posting.objects.filter(corp_id=com_info.corp_id)
+            # job_posting_jobs = Jobposting_job.objects.filter(Jobposting_id=job_postings.jobposting_id)
+            job_postings = Job_posting.objects.filter(jobposting_id=jobposting_id)
+            job_posting_jobs = Jobposting_job.objects.filter(jobposting_id=jobposting_id)
+
+            context['corp_nm'] = com_info.first().corp_nm
+            context['job_postings'] = job_postings.values()[0]
+            context['job_posting_job']=job_posting_jobs.values()[0]
+            return JsonResponse(context, status=200)
+
+        except django.db.utils.OperationalError :
+            return JsonResponse({'err':"테이블 없음"}, status=400)
+        except Exception as err:
+            return JsonResponse({"err": err})
+
+
+def recruit_positions(request, jobposting_id): # 수정필요
+    context={}
+    if request.method == "GET":
+        try:
+            req_corp_nm = request.GET.get('corp_nm', None)
+            print(req_corp_nm)
+            if req_corp_nm is None :
+                return redirect('/')
+            #     return JsonResponse({"err" : "값을 입력해 주세요"}, status=400)
+            
+            com_info = Corporation.objects.filter(corp_nm=req_corp_nm)
+            job_postings = Job_posting.objects.filter(jobposting_id=jobposting_id)
+            job_posting_jobs = Jobposting_job.objects.filter(jobposting_id=job_postings.first().jobposting_id)
+            
+            context['corp_nm'] = com_info.first().corp_nm
+            context['job_postings'] = job_postings.values()[0]
+            context['job_posting_job']=job_posting_jobs.values()[0]
+            return JsonResponse(context, status=200)
+        except django.db.utils.OperationalError :
+            return JsonResponse({'err':"테이블 없음"}, status=400)
+        except Exception as err:
+            return JsonResponse({"err": err})
---
+++ zazinmori_server/models.py
@@ -0,0 +1,199 @@
+from django.db import models
+
+
+class Concept(models.Model):
+    concept_id = models.AutoField(primary_key=True)
+    regi_code = models.CharField(max_length=45, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    concept = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'concept'
+
+
+class Corp_finance(models.Model):
+    finance_id = models.AutoField(primary_key=True)
+    regi_code = models.CharField(max_length=45, blank=True, null=True)
+    year = models.CharField(max_length=45, blank=True, null=True)
+    stock_code = models.CharField(max_length=45, blank=True, null=True)
+    total_assets = models.CharField(max_length=45, blank=True, null=True)
+    total_debt = models.CharField(max_length=45, blank=True, null=True)
+    total_capital = models.CharField(max_length=45, blank=True, null=True)
+    total_sales = models.CharField(max_length=45, blank=True, null=True)
+    operating_income = models.CharField(max_length=45, blank=True, null=True)
+    net_income = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'corp_finance'
+
+
+class Corporation(models.Model):
+    regi_code = models.TextField(primary_key=True)
+    # regi_code = models.TextField(blank=True, null=True)
+    corp_nm = models.TextField(blank=True, null=True)
+    corp_nm_eng = models.TextField(blank=True, null=True)
+    category = models.TextField(blank=True, null=True)
+    ceo_nm = models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    phn_no = models.TextField(blank=True, null=True)
+    est_dt = models.TextField(blank=True, null=True)
+    stock_code = models.TextField(blank=True, null=True)
+    stock_nm = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'corporation'
+
+
+class Cvletter_items(models.Model):
+    items_id = models.BigIntegerField()
+    jobs_id = models.BigIntegerField()
+    question = models.TextField(blank=True, null=True)
+    word = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cvletter_items'
+
+
+
+class Failcvletter(models.Model):
+    cvletter = models.AutoField(primary_key=True)
+    regi_code = models.CharField(max_length=45, blank=True, null=True)
+    corp_nm = models.TextField(blank=True, null=True)
+    job = models.TextField(blank=True, null=True)
+    recruit_date = models.TextField(blank=True, null=True)
+    new_or_exp = models.TextField(blank=True, null=True)
+    q1 = models.TextField(blank=True, null=True)
+    a1 = models.TextField(blank=True, null=True)
+    q2 = models.TextField(blank=True, null=True)
+    a2 = models.TextField(blank=True, null=True)
+    q3 = models.TextField(blank=True, null=True)
+    a3 = models.TextField(blank=True, null=True)
+    q4 = models.TextField(blank=True, null=True)
+    a4 = models.TextField(blank=True, null=True)
+    q5 = models.TextField(blank=True, null=True)
+    a5 = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'failcvletter'
+
+
+class Jobposting(models.Model):
+    jobposting_id = models.BigIntegerField()
+    corp_nm = models.TextField(blank=True, null=True)
+    period = models.TextField(blank=True, null=True)
+    start_time = models.TextField(blank=True, null=True)
+    end_time = models.TextField(blank=True, null=True)
+    posting_detail = models.TextField(blank=True, null=True)
+    posting_type = models.TextField(blank=True, null=True)
+    url = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'jobposting'
+
+
+class Jobposting_jobs(models.Model):
+    jobs_id = models.BigIntegerField()
+    jobposting_id = models.BigIntegerField()
+    job = models.TextField(blank=True, null=True)
+    new_or_exp = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'jobposting_jobs'
+
+
+class Passcvletter(models.Model):
+    passcvletter_code = models.AutoField(primary_key=True)
+    regi_code = models.CharField(max_length=45, blank=True, null=True)
+    corp_nm = models.TextField(blank=True, null=True)
+    job = models.TextField(blank=True, null=True)
+    recruit_date = models.TextField(blank=True, null=True)
+    new_or_exp = models.TextField(blank=True, null=True)
+    q1 = models.TextField(blank=True, null=True)
+    a1 = models.TextField(blank=True, null=True)
+    q2 = models.TextField(blank=True, null=True)
+    a2 = models.TextField(blank=True, null=True)
+    q3 = models.TextField(blank=True, null=True)
+    a3 = models.TextField(blank=True, null=True)
+    q4 = models.TextField(blank=True, null=True)
+    a4 = models.TextField(blank=True, null=True)
+    q5 = models.TextField(blank=True, null=True)
+    a5 = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'passcvletter'
+
+
+class Topic(models.Model):
+    topic_id = models.AutoField(primary_key=True)
+    regi_code = models.CharField(max_length=45, blank=True, null=True)
+    corp_nm = models.CharField(max_length=45, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    url = models.CharField(max_length=150, blank=True, null=True)
+    keyword1 = models.CharField(max_length=45, blank=True, null=True)
+    keyword2 = models.CharField(max_length=45, blank=True, null=True)
+    keyword3 = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'topic'
+
+
+class User_cvletter(models.Model):
+    user_cvletter_id = models.AutoField(primary_key=True)
+    member_id = models.CharField(max_length=45, blank=True, null=True)
+    written_date = models.TextField(blank=True, null=True)
+    q1 = models.TextField(blank=True, null=True)
+    q2 = models.TextField(blank=True, null=True)
+    q3 = models.TextField(blank=True, null=True)
+    q4 = models.TextField(blank=True, null=True)
+    q5 = models.TextField(blank=True, null=True)
+    q6 = models.TextField(blank=True, null=True)
+    q7 = models.TextField(blank=True, null=True)
+    q8 = models.TextField(blank=True, null=True)
+    a1 = models.TextField(blank=True, null=True)
+    a2 = models.TextField(blank=True, null=True)
+    a3 = models.TextField(blank=True, null=True)
+    a4 = models.TextField(blank=True, null=True)
+    a5 = models.TextField(blank=True, null=True)
+    a6 = models.TextField(blank=True, null=True)
+    a7 = models.TextField(blank=True, null=True)
+    a8 = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user_cvletter'
+
+
+class User_scrap(models.Model):
+    scrap_id = models.AutoField(primary_key=True)
+    member_id = models.CharField(max_length=45, blank=True, null=True)
+    jobposting_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user_scrap'
+
+
+class User_info(models.Model):
+    member_id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=45, blank=True, null=True)
+    passwd = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    birth = models.CharField(max_length=45, blank=True, null=True)
+    gender = models.CharField(max_length=45, blank=True, null=True)
+    reg_date = models.CharField(max_length=45, blank=True, null=True)
+    update_date = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user_info'
