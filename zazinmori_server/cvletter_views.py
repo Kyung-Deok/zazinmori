991d7c0bba3b763187dcf85fccfb7ba8931762e4
nijes <monochrome05@gmail.com>
2022-09-22 19:57:52 +0900

    '2209221957'

---
+++ zazinmori_server/cvletter_views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import bcrypt
+import django
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+from django.http import JsonResponse
+
+from .models import *
+
+def cvletter_write(request):
+    if request.method == "GET":
+        return render(request, '자소서 작성 페이지.html')
+    elif request.method == "POST":
+        context={}
+        try:
+            pass
+        except:
+            pass
+        
