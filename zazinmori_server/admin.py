79e4a7e2f777803871ac444f8bbecc938838b785
nijes <monochrome05@gmail.com>
2022-09-22 13:52:48 +0900

    'edit_admin.py'

---
+++ zazinmori_server/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(User_info)
+admin.site.register(User_scrap)
+admin.site.register(Corporation)
+admin.site.register(Corp_finance)
+admin.site.register(Concept)
+admin.site.register(Topic)
+admin.site.register(Jobposting)
+admin.site.register(Passcvletter)
+admin.site.register(Jobposting_jobs)
+admin.site.register(Cvletter_items)
+admin.site.register(User_cvletter)
